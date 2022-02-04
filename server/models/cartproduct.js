6db1c51b1d7a839562e8f6c11d19e37aca7182ca
ihzaw <ihzanantama@gmail.com>
2022-02-05 00:00:14 +0800

    create CartProduct table, add many-to-many relation to Cart and Product

--- server/models/cartproduct.js
+++ server/models/cartproduct.js
@@ -3,29 +3,21 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Cart extends Model {
+  class CartProduct extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Cart.hasOne(models.User, {foreignKey: 'userId'}),
-      Cart.belongsToMany(models.Product, {through: models.CartProduct})
+      // define association here
     }
   }
-  Cart.init({
-    userId: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      references: {
-        model: 'Users',
-        key: 'id'
-      }
-    }
+  CartProduct.init({
+    id: DataTypes.INTEGER
   }, {
     sequelize,
-    modelName: 'Cart',
+    modelName: 'CartProduct',
   });
-  return Cart;
+  return CartProduct;
 };
