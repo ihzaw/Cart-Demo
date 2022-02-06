10b5963c582236c0278d1dc75ca7d8ca94cdca9f
ihzaw <ihzanantama@gmail.com>
2022-02-06 13:35:04 +0800

    add card component, re-layouting

--- client/cart-demo/src/App.js
+++ client/cart-demo/src/App.js
@@ -1,16 +1,78 @@
-import Cart from "./assets/cart.svg";
+import { ReactComponent as Cart } from "./assets/cart.svg";
 import { Card } from "./components/Card";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function App() {
+  const baseUrl = "http://localhost:3000";
+  const [products, setProducts] = useState([]);
+  const [cartProducts, setCartProducts] = useState([]);
+  // const [adding, setAdding] = useState(false);
+  const [page, setPage] = useState('Home')
+
+  const fetchProducts = () => {
+    axios
+      .get(`${baseUrl}/products`)
+      .then(({ data }) => {
+        console.log(data);
+        setProducts(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const addToCart = (productId) => {
+    axios.post(`${baseUrl}/carts/1`, { productId })
+    .then(({ data }) => {
+      const payload = [
+        ...cartProducts
+      ]
+      payload.push(data.response)
+      setCartProducts(payload)
+    })
+    .catch(err => console.log(err))
+  }
+
+  const fetchCart = () => {
+    axios
+      .get(`${baseUrl}/carts/1`)
+      .then(({ data }) => {
+        console.log(data);
+        setCartProducts(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  useEffect(() => {
+    fetchProducts();
+    fetchCart()
+  }, []);
+
   return (
-    <div className="App">
-        <nav className="mb-8 w-full grid grid-cols-3 h-16 bg-red-200 justify-end px-6">
-          <div className="col-start-3 flex justify-center p-2">
-            <img className="object-fit" src={Cart} />
-          </div>
-        </nav>
-        <div className="px-8 grid grid-cols-3 grid-rows-3 text-gray-900">
-          <Card />
-        </div>
+    <div className="App bg-slate-100 font-mono">
+      <div className="fixed bottom-4 left-4 col-start-3 flex justify-center items-center p-7 h-28 w-28 bg-yellow-400 rounded-full hover:bg-yellow-500 cursor-pointer" onClick={() => setPage('Cart')}>
+        {cartProducts.length !== 0 && <div
+          className={`absolute top-2 right-0 bg-[#ff0000] text-white h-8 w-8 font-bold rounded-full flex justify-center items-center`}
+        >
+          {cartProducts.length}
+        </div>}
+        <Cart className="h-full w-full stroke-2" fill="white" />
+      </div>
+      {page === 'Home' && <div className="grid grid-cols-3 px-16 text-gray-900">
+        {products.map((product) => {
+          return <Card product={product} key={product.id} addToCart={addToCart} />;
+        })}
+        {/* <Card product={products[0]} /> */}
+      </div>}
+      {page === 'Cart' && <div className="grid grid-cols-3 px-16 text-gray-900">
+        {cartProducts.map((product, index) => {
+          return <Card product={product} key={index} addToCart={addToCart} page={page} />;
+        })}
+        {/* <Card product={products[0]} /> */}
+      </div>}
     </div>
   );
 }
--- client/cart-demo/src/components/Card.js
+++ client/cart-demo/src/components/Card.js
@@ -1,10 +1,39 @@
-export const Card = () => {
+import { ReactComponent as Plus } from '../assets/plus.svg';
+import { ReactComponent as Minus } from '../assets/minus.svg';
+
+export const Card = ({ product, addToCart, page }) => {
   return (
-    <div className="m-4 grid grid-rows-5 grid-cols-3 shadow-xl rounded-lg">
-      <div className="bg-blue-200 row-span-3 col-span-3">image</div>
-      <div className="p-2 col-span-2">product name</div>
-      <div className="bg-yellow-200 row-span-2">add to cart</div>
-      <div className="p-2 bg-slate-200 col-span-2">product price</div>
+    <div className="h-96 grid grid-rows-4 m-8 shadow-xl rounded-xl bg-white">
+      <div className="row-span-3 flex justify-center items-center overflow-hidden bg-[#e8e7e5] rounded-xl">
+        <img
+          className="object-contain h-full"
+          src={product.product_imgUrl}
+          alt="img"
+        />
+      </div>
+      <div className="flex rounded-b-xl">
+        <div className="w-2/3 h-full text-sm">
+          <div className="p-4 h-1/2 text-base font-bold">{product.product_name}</div>
+          <div className="p-4 h-1/2">{`Rp. ${product.product_price}`}</div>
+        </div>
+        {page !== 'Cart' && <div className="h-full w-1/3 flex justify-center items-center" onClick={() => addToCart(product.id)}>
+          <div className="w-20 h-20 p-4 justify-center flex">
+            <div className="bg-green-400 hover:bg-green-500 h-full w-full rounded-full cursor-pointer flex justify-center items-center text-4xl font-bold text-white">
+              <Plus fill="white" className="h-5 w-5"/>
+            </div>
+          </div>
+        </div>}
+        {page === 'Cart' && <div className="h-full w-1/3 flex justify-center items-center" onClick={() => addToCart(product.id)}>
+          <div className="w-20 h-20 p-4 justify-center flex">
+            <div className="bg-red-400 hover:bg-red-500 h-full w-full rounded-full cursor-pointer flex justify-center items-center text-4xl font-bold text-white">
+              <Minus fill="white" className="h-5 w-5"/>
+            </div>
+          </div>
+        </div>}
+      </div>
     </div>
+    // <div className="flex h-full w-1/3">
+    //   <div className="bg-blue-200 h-full w-full p-4 mx-4"></div>
+    // </div>
   );
 };
